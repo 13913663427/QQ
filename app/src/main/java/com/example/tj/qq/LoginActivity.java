796fbe0227238536619c988c04ad2b6eb891b195
13913663427 <763910972@qq.com>
2018-11-01 21:56:33 +0800

    11.1晚上修改注册界面

---
+++ app/src/main/java/com/example/tj/qq/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.tj.qq;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+    private Button loginBTN;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBTN = findViewById(R.id.btn_login);
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
