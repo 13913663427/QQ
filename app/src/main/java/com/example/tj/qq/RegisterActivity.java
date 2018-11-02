e6320e911bced0da7d8cb3055c2483ed0260de4d
13913663427 <763910972@qq.com>
2018-11-02 14:14:00 +0800

    11.2更新

--- app/src/main/java/com/example/tj/qq/RegisterActivity.java
+++ app/src/main/java/com/example/tj/qq/RegisterActivity.java
@@ -1,12 +1,24 @@
 package com.example.tj.qq;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends Activity {
+    private TextView tv_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        tv_register = findViewById(R.id.text_back);
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
