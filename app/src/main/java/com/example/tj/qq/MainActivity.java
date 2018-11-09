ae1f3736d5ff5b1d3869292d36cd708a5e88e450
qinzb <673565935@qq.com>
2018-11-09 16:58:29 +0800

    QQ5.0特效+Fragment

--- app/src/main/java/com/example/tj/qq/MainActivity.java
+++ app/src/main/java/com/example/tj/qq/MainActivity.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
+
+import com.example.tj.qq.fragment.LeftMenuFragment;
+import com.example.tj.qq.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-    private LinearLayout leftMenu;
-    private LinearLayout mainMenu;
+    private FrameLayout leftMenu;
+    private FrameLayout mainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +22,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initView();
+        initListener();
+    }
+
+    private void initView() {
+        leftMenu = findViewById(R.id.fl_left_menu);
+        mainMenu = findViewById(R.id.fl_main);
+        drawerLayout = findViewById(R.id.am_drawer_layout);
+
+        // 初始化Fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fl_left_menu, new LeftMenuFragment())
+                .add(R.id.fl_main, new MainFragment())
+                .commit();
+    }
+
+    private void initListener() {
+
+        // QQ5.0特效
         drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
                 super.onDrawerSlide(drawerView, slideOffset);
 
-                leftMenu.setScaleX(0.2f + slideOffset);
-                leftMenu.setScaleY(0.2f + slideOffset);
-                leftMenu.setAlpha(0.6f + 0.4f * slideOffset);
+                float leftScale = 0.6f + 0.4f * slideOffset;
+                leftMenu.setScaleX(leftScale);
+                leftMenu.setScaleY(leftScale);
+                leftMenu.setAlpha(leftScale);
 
-                mainMenu.setScaleX(1 - 0.4f * slideOffset);
-                mainMenu.setScaleY(1 - 0.4f * slideOffset);
+                float mainScale = 1 - 0.4f * slideOffset;
+                mainMenu.setScaleX(mainScale);
+                mainMenu.setScaleY(mainScale);
                 mainMenu.setTranslationX(drawerView.getMeasuredWidth() * slideOffset * 0.7f);
             }
         });
-    }
-
-    private void initView() {
-        leftMenu = findViewById(R.id.left_menu);
-        mainMenu = findViewById(R.id.main_menu);
-        drawerLayout = findViewById(R.id.am_drawer_layout);
     }
 }
