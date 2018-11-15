1331d4423859c84d4b8d5b806bd0f7d6dc22be92
13913663427 <763910972@qq.com>
2018-11-15 22:21:37 +0800

    11.15晚上权限加上服务端与本地数据格式转换

---
+++ app/src/main/java/com/example/tj/qq/activity/JSONActivity.java
@@ -0,0 +1,50 @@
+package com.example.tj.qq.activity;
+
+import android.util.Log;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.example.tj.qq.R;
+import com.example.tj.qq.item.UserItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_drawer_layout;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+        try {
+            List<UserItem> list = new ArrayList<>();
+            UserItem userItem = new UserItem("张三", "123456");
+            list.add(userItem);
+            userItem = new UserItem("张三1", "123456");
+            list.add(userItem);
+            userItem = new UserItem("张三2", "123456");
+            list.add(userItem);
+            JSONArray ja = JSON.parseArray(JSON.toJSONString(list));
+            String backStr = ja.toJSONString();
+
+            List<UserItem> users = JSON.parseArray(backStr, UserItem.class);
+            Log.e(TAG, users.toString());
+            Log.e(TAG, backStr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
