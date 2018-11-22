c1d3c6e7ad6c9dc9ce7c9d533bac4204f258eb2b
13913663427 <763910972@qq.com>
2018-11-22 17:39:31 +0800

    11.22下班提交获取短信验证码

--- app/src/main/java/com/example/tj/qq/screenadaptation/BaseApplication.java
+++ app/src/main/java/com/example/tj/qq/screenadaptation/BaseApplication.java
@@ -3,6 +3,7 @@ package com.example.tj.qq.screenadaptation;
 import android.app.Application;
 
 import com.lzy.okgo.OkGo;
+import com.mob.MobSDK;
 
 import me.jessyan.autosize.AutoSizeConfig;
 import me.jessyan.autosize.unit.Subunits;
@@ -12,6 +13,9 @@ public class BaseApplication extends Application {
     public void onCreate() {
         super.onCreate();
         //对单位的自定义配置, 请在 App 启动时完成
+
+        MobSDK.init(this);//初始化MobSDK
+
         configUnits();
         OkGo.getInstance().init(this);
     }
